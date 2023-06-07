0d05ca1f9e06874cfab7b45a610a054456d08c55
paulohldev <paulohl.dev@gmail.com>
2023-06-07 12:45:35 -0300

    arrumando a conexao do bd

--- sql.js
+++ sql.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('refugioartesanal', 'root', 'admin', {
+const sequelize = new Sequelize('refugioartesanal', 'aluno', 'ifpe2023', {
   host: 'localhost',
   dialect: 'mysql',
   logging: true,
