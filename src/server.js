9de1f5ce05ae2a8980bbbe2bc4fc0d98b7f4cb43
JaimeFcls <119618310+JaimeFcls@users.noreply.github.com>
2023-06-13 14:06:45 -0300

    atualização nome rota

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Inicia o banco de dados
-require('./database/index');
+require("./database/index");
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Importação das rotas e inicializaçao com o meto app.use()
-const usuarioRoutes = require('./routes/usuarioRoutes');
-const categoriaRoutes = require('./routes/categoriaRoutes');
+const usuarioRoutes = require("./routes/usuarioRoutes");
+const categoriaRoutes = require("./routes/categoriaRoutes");
 
-const produtosRoutes = require('./routes/produtosRoutes');
-const pedidosRoutes = require('./routes/pedidosRoutes');
+const produtosRoutes = require("./routes/ProdutosRoutes");
+const pedidosRoutes = require("./routes/pedidosRoutes");
 
-app.use('/usuarios', usuarioRoutes);
-app.use('/categorias', categoriaRoutes);
-app.use('/produtos', produtosRoutes);
-app.use('/pedidos', pedidosRoutes);
+app.use("/usuarios", usuarioRoutes);
+app.use("/categorias", categoriaRoutes);
+app.use("/produtos", produtosRoutes);
+app.use("/pedidos", pedidosRoutes);
 
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
-  console.log('Servidor iniciado: http://localhost:3000 👍');
+  console.log("Servidor iniciado: http://localhost:3000 👍");
 });
