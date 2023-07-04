2561ff020ce0a8932295b67a4e137a95d18a2fdc
paulohldev <paulohl.dev@gmail.com>
2023-07-04 17:15:02 -0300

    criado middleware de autorização

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,9 @@ module.exports = async function auth(req, res, next) {
   const auth = req.headers.authorization;
 
   if (!auth) {
-    return res.status(401).json({ mensagem: 'Você precisa estar logado.' });
+    return res
+      .status(401)
+      .json({ mensagem: 'Você não tem permissão para acessar.' });
   }
   const [, token] = auth.split(' ');
 
@@ -14,7 +16,7 @@ module.exports = async function auth(req, res, next) {
 
     const { id, email } = dados;
 
-    const usuario = Usuario.findOne({ where: { id, email } });
+    const usuario = await Usuario.findOne({ where: { id, email } });
 
     if (!usuario) {
       return res.status(401).json({ mensagem: 'Usuário inválido' });
